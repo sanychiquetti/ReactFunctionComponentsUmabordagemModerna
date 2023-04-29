bfeec8099fee3ec8fd46adf8554124ecbd1df169
sanychiquetti <sanymara77@hotmail.com>
2023-04-29 20:05:18 -0300

    importando components

---
+++ meu-formulario-react/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component, Fragment } from 'react'
+import './App.css';
+import FormularioCadastro from './components/FormularioCadasro/FormularioCadastro';
+
+class App extends Component {
+
+  render(){
+    return (
+      <Fragment>
+        <h1>Formulário de Cadastro</h1>
+        <FormularioCadastro/>
+      </Fragment>
+
+    );
+  }
+}
+
+export default App;
