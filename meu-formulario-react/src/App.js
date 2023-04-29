877064e6094589114792a4c0e320f502dd9e191f
sanychiquetti <sanymara77@hotmail.com>
2023-04-29 20:27:45 -0300

    arrumando pastas

--- meu-formulario-react/src/App.js
+++ meu-formulario-react/src/App.js
@@ -1,16 +1,14 @@
-import React, { Component, Fragment } from 'react'
-import './App.css';
-import FormularioCadastro from './components/FormularioCadasro/FormularioCadastro';
+import React, { Component, Fragment } from "react";
+import "./App.css";
+import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
 
 class App extends Component {
-
-  render(){
+  render() {
     return (
       <Fragment>
-        <h1>Formulário de Cadastro</h1>
-        <FormularioCadastro/>
+        <h1>Formulário de cadastro</h1>
+        <FormularioCadastro />
       </Fragment>
-
     );
   }
 }
